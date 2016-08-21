3defff437134b83475d6ab29f7d1cc75d0c46e85
Susana Isaza <susana@Susanas-MacBook-Air.local>
2016-08-21 11:28:42 +0200

    added if/else for cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,17 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+	alert("Sorry, try again")
+} if else (cardTwo === cardThree) {
+	alert("Sorry, try again")
+} if else (cardTwo ==== cardOne) {
+	alert("Match found")
+} if else (cardOne === cardThree) {
+	alert("Sorry, try again")
+} if else (cardOne === cardFour) {
+	alert("Sorry, try again")
+} if else (cardThree === cardFour) {
+	alert("Match found")
+}

bc6d771ea0534f6a00b6bdb5bf89653e82925179
Susana <susisaza@gmail.com>
2016-09-18 12:35:05 -0400

    final changes

--- starter-code/main.js
+++ starter-code/main.js
@@ -34,34 +34,34 @@ function createBoard() {
 	for (var i = 0; i < cards.length; i++) {
 		var newCard = document.createElement('div');
 		newCard.className = 'card';
-		newBoard.appendChild(newCard);
-		newCard.setAttribute('data-card', cards[i]);
+		newBoard.appendChild(newCard); // Add cards to the board
+		newCard.setAttribute('data-card', cards[i]); 
 		newCard.addEventListener('click', isTwoCards);
 	}
 }
-function isTwoCards() {
-	cardsInPlay.push(this.getAttribute('data-card'));
-	console.log(this.getAttribute('data-card'));
-		if(this.getAttribute('data-card') === 'queen') {
+function isTwoCards() {  // Compares the value of the 'data-card' selected, and assigns it an img that appears with the event listener 
+	cardsInPlay.push(this.getAttribute('data-card')); 
+	console.log(this.getAttribute('data-card'));  
+		if(this.getAttribute('data-card') === 'queen') { 
 			this.innerHTML = "<img src='queen image.png' />";
 		}
 		else {
 			this.innerHTML = "<img src='king image.png' />";
 		}
 	if (cardsInPlay.length === 2) {
-		isMatch(cardsInPlay);
-		cardsInPlay = [];
+		isMatch(cardsInPlay); // Calls function isMatch with cardsInPlay as argument
+		cardsInPlay = []; // Clears the cardsInPlay array after two cards are selected
 	}
 }
-function isMatch(cardsInPlay) {
+function isMatch(cardsInPlay) {  // Issue: alert with result appears before second card turns around
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("Match found");
 	}
 	else {
 		alert("No match here, try again");
 	}
-	for (var i = 0; i < cards.length; i++) {
-			document.getElementsByClassName('card')[i].innerHTML = " ";
+	for (var i = 0; i < cards.length; i++) { 
+			document.getElementsByClassName('card')[i].innerHTML = " "; // To hide the img assigned, leaving it with the default div style and start over
 		}
 }
 createBoard();
